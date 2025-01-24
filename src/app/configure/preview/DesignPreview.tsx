0f4ef6f8b44e6327ce5f3a289c39c283014de8ec
Dheeraj Goswami <132127084+dheeujxs@users.noreply.github.com>
2025-01-24 17:16:31 +0530

    Update DesignPreview.tsx

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -51,16 +51,7 @@ const DesignPreview = ({configuration}: {configuration: Configuration}) => {
             })
         }
     })
-    const handleCheckout = () => {
-        if(user){
-            // create payment session
-            createPaymentSession({configId: id})
-        } else {
-            // need to log in
-            localStorage.setItem('configurationId', id)
-            setIsLoginModalOpen(true)
-        }
-    }
+   
 
     return(
         <>
@@ -150,7 +141,7 @@ const DesignPreview = ({configuration}: {configuration: Configuration}) => {
                 </div>
             </div>
             <div className='mt-8 flex justify-end pb-12'>
-                <Button onClick={() => handleCheckout()} 
+                <Button onClick={() => createPaymentSession({configId: configuration.id})}
                     className='px-4 sm:px-6 lg:px-8'>Check out 
                 <ArrowRight className='h-4 w-4 ml-1.5 inline' />
                 </Button>
